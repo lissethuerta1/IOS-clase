88de0a9be15d56d6645b97e82cf3bd054eb5972b
Facultad de Contaduría y Administración <facultaddecontaduriayadministracion@MacBook-10.local>
2026-08-20 14:47:30 -0600

    ej 2

---
+++ Ej2-20-08-26.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let width = 10
+let height = 20
+let area = width * height
+print(area)
+
+let roomArea = area / 2
+print(roomArea)
+
+let perimeter = width + width + height + height
+print(perimeter)

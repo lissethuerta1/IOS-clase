94457e4c40726fec78ced1463e9f1fd267d59cec
Lisset <Liss@gmail.com>
2026-09-08 14:56:08 -0600

    ejercicio 3

---
+++ 08-09-2026/Ejercicio3 8Nov/Ejercicio3 8Nov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio3 8Nov
+//
+//  Created by Facultad de Contaduría y Administración on 08/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

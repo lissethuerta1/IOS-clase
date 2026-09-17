d8e477eb24b1efedee80442191dce2cd4fc718ea
Lisset <Liss@gmail.com>
2026-09-17 13:57:31 -0600

    Clase 10 nov

---
+++ Ejercicio1 10Nov/Ejercicio1 10Nov/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejercicio1 10Nov
+//
+//  Created by Facultad de Contaduría y Administración on 10/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func OnTapButton1(_ sender: Any) {
+        print("Click button 1")
+
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
